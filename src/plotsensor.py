63469187a32a0f8efcb39a3478ca33b2670366a8
Muhammad Labiyb Afakh <labiybafakh@gmail.com>
2022-07-14 06:38:32 +0900

    experimental data

--- src/plotsensor.py
+++ src/plotsensor.py
@@ -11,26 +11,32 @@ from geometry_msgs.msg import WrenchStamped
 
 class PlotSensor:
     def __init__(self) -> None:
-        force = np.zeros(shape=3, dtype=np.float32)
-        moment= np.zeros(shape=3, dtype=np.float32)
-
+        self.force_x = []
+        self.force_y = []
+        self.force_z = []
+        self.time    = []
+        # self.force = np.zeros(3, dtype=np.float32)
+        # self.moment= np.zeros(3, dtype=np.float32)
         # self.force = structure()
-        # self.force.x = np.zeros(dtype=np.float32)
-        # self.force.y = np.zeros(dtype=np.float32)
-        # self.force.z = np.zeros(dtype=np.float32)        
+        # self.force.x = []
+        # self.force.y = []
+        # self.force.z = []        
 
         # self.moment = structure()
-        # self.moment.x = np.zeros(dtype=np.float32)
-        # self.moment.y = np.zeros(dtype=np.float32)
-        # self.moment.z = np.zeros(dtype=np.float32)
-
+        # self.moment.x = []
+        # self.moment.y = []
+        # self.moment.z = []
 
         self.sub_force      = rospy.Subscriber('leptrino_force_torque/force_torque', WrenchStamped, self.callback_force_sensor)
     
     def callback_force_sensor(self, force_data):
-        force = np.append(  force_data.wrench.force.x,
-                            force_data.wrench.force.y,
-                            force_data.wrench.force.z )
+        self.force_x.append(force_data.wrench.force.x)
+        self.force_y.append(force_data.wrench.force.y)
+        self.force_z.append(force_data.wrench.force.z)
+        # self.force = np.append(self.force,  [force_data.wrench.force.x,
+        #                     force_data.wrench.force.y,
+        #                     force_data.wrench.force.z] )
+        
 
 class OrnibiBot:
     def __init__(self):
@@ -41,16 +47,26 @@ class OrnibiBot:
         self.last_time      = rospy.Time.now()
         self.deltaTime      = rospy.Time.now()
 
-
 if __name__ == '__main__':
-
-    sensor = PlotSensor()
+    
     ornibibot = OrnibiBot()
+    sensor = PlotSensor()
 
     try:
         while not rospy.is_shutdown():
-            rospy.rospy.loginfo("Force:{}".format(sensor.force))
+            # rospy.loginfo("Force:{}".format(sensor.force))
             ornibibot.rate.sleep()
-                    
-    
+            # ornibibot.time = np.append(ornibibot.time, [rospy.Time.now().to_sec(),rospy.Time.now().to_sec(),rospy.Time.now().to_sec()])
+        
+        graph.plot(sensor.time, sensor.force_x)
+        graph.plot(sensor.time, sensor.force_y)
+        graph.plot(sensor.time, sensor.force_z)
+        graph.xlabel('Time')
+        graph.ylabel('Force')
+        graph.title('Static Force')
+        graph.legend()
+        graph.tight_layout()
+        graph.show()
+
+
     except rospy.ROSInterruptException : pass
